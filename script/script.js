4d8482c7484d8f37632f4c3fce03e9f749fece9f
berezinke <berezinke@yandex.ru>
2022-04-20 20:53:52 +0300

    V 1.2

--- script/script.js
+++ script/script.js
@@ -1,71 +1,35 @@
-let button_changeData = document.querySelector('#button_edit');
-let button_NochangeData = document.querySelector('#button_NoSave');
-let popup_styles = document.querySelector('.popup');
+let popupStyles = document.querySelector('.popup');
+let objectNameChange = document.querySelector('#change-name');
+let objectProfeccionChange = document.querySelector('#change-profeccion');
 
-let object_name = document.querySelector('#nameScientist');
-let object_profeccion = document.querySelector('#profeccionScientist');
-let nameInput = object_name.textContent.trim();
-let jobInput = object_profeccion.textContent.trim();
-
-let object_name_change = document.querySelector('#change-name');
-let object_profeccion_change = document.querySelector('#change-profeccion');
-let button_submit = document.querySelector('#ButtonSubmit');
-
-button_changeData.onclick = function() {
-    popup_styles.classList.add('popup_opened');
-    object_name_change.value = nameInput;
-    object_profeccion_change.value = jobInput;
+// Редактирование в попАп
+function popUpOpened() {
+    popupStyles.classList.add('popup_opened');
 };
-button_NochangeData.onclick = function() {
-    popup_styles.classList.remove('popup_opened');
+function popUpClosed() {
+    popupStyles.classList.remove('popup_opened');
 };
-button_submit.onclick = function() {
+document.querySelector('#button_edit').addEventListener('click', function() {
+    popUpOpened();
+    objectNameChange.value = document.querySelector('#nameScientist').textContent.trim();
+    objectProfeccionChange.value = document.querySelector('#profeccionScientist').textContent.trim();
+});
+document.querySelector('#button_NoSave').addEventListener('click', popUpClosed);
+document.querySelector('#ButtonSubmit').addEventListener('click', function() {
     event.preventDefault();
-    object_name.textContent = object_name_change.value;
-    object_profeccion.textContent = object_profeccion_change.value;
-    nameInput = object_name.textContent;
-    jobInput = object_profeccion.textContent;
-    popup_styles.classList.remove('popup_opened');
+    document.querySelector('#nameScientist').textContent = objectNameChange.value;
+    document.querySelector('#profeccionScientist').textContent = objectProfeccionChange.value;
+    popUpClosed();
+});
+// Конец редактирования в попАп
+
+// Выделение понравившихся
+let arrPictures = document.querySelectorAll('#button_like');
+function isLiked () {
+    for (let i = 0; i < arrPictures.length; i++) {
+        arrPictures[i].addEventListener('click', function() {
+            arrPictures[i].classList.toggle('element__button-liked_activ')});
+    };
 }
-
-let button_likedFirst = document.querySelector('#button_like_first');
-let isLiked_first = false;
-button_likedFirst.onclick = function() {
-    isLiked_first = !isLiked_first;
-    button_likedFirst.classList.toggle('element__button-liked_black');
-};
-
-let button_likedSecond = document.querySelector('#button_like_second');
-let isLiked_Second = false;
-button_likedSecond.onclick = function() {
-    isLiked_Second = !isLiked_Second;
-    button_likedSecond.classList.toggle('element__button-liked_black');
-};
-
-let button_likedThird = document.querySelector('#button_like_third');
-let isLiked_Third = false;
-button_likedThird.onclick = function() {
-    isLiked_Third = !isLiked_Third;
-    button_likedThird.classList.toggle('element__button-liked_black');
-};
-
-let button_likedForth = document.querySelector('#button_like_forth');
-let isLiked_Forth = false;
-button_likedForth.onclick = function() {
-    isLiked_Forth = !isLiked_Forth;
-    button_likedForth.classList.toggle('element__button-liked_black');
-};
-
-let button_likedFifth = document.querySelector('#button_like_fifth');
-let isLiked_Fifth = false;
-button_likedFifth.onclick = function() {
-    isLiked_Fifth = !isLiked_Fifth;
-    button_likedFifth.classList.toggle('element__button-liked_black');
-};
-
-let button_likedSixth = document.querySelector('#button_like_sixth');
-let isLiked_Sixth = false;
-button_likedSixth.onclick = function() {
-    isLiked_Sixth = !isLiked_Sixth;
-    button_likedSixth.classList.toggle('element__button-liked_black');
-};
+isLiked();
+// Конец выделения понравившихся
