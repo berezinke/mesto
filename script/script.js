f906e9b488e5daf0ba855289cfa968e4af7ddd02
berezinke <berezinke@yandex.ru>
2022-04-28 19:42:39 +0300

    V 1.4

--- script/script.js
+++ script/script.js
@@ -3,107 +3,77 @@ const nameInKusto = document.querySelector('#nameScientist');
 const infoInKusto = document.querySelector('#profeccionScientist');
 
 const buttonAddPicture = document.querySelector('.profile__add-button'); // Новая карточка
-const nameInCard = document.querySelector('#namePicNew');
-const infoInCard = document.querySelector('#placePicNew');
+let nameInCard = document.querySelector('#namePicNew');
+let infoInCard = document.querySelector('#placePicNew');
 const formAddCard = document.querySelector('#cardAdd');
 
 const cardsPosition = document.querySelector('.elements'); // Создание карточек
-const cardTemplate = document.querySelector('#elementTemplate').content;
+const cardTemplate = document.querySelector('#elementTemplate').content.querySelector('.element');
 
-let arrPictures = [];
+const openPopInfo = document.querySelector('#popEdit');
+const openPopCard = document.querySelector('#popAddPicture');
+const openPopPic = document.querySelector('#popShowPicture');
 
-const initialCards = [
-   {
-     name: 'Архыз',
-     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-   },
-   {
-     name: 'Челябинская область',
-     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-   },
-   {
-     name: 'Иваново',
-     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-   },
-   {
-     name: 'Камчатка',
-     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-   },
-   {
-     name: 'Холмогорский район',
-     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-   },
-   {
-     name: 'Байкал',
-     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-   }
- ];
+const buttonClosePopupPicture = document.querySelectorAll('.popup__nosave-button');
 
 // Открытие, редактирование и закрытие попАп
-function popUpOpened(idPop) {
-   const popupStyles = document.querySelector(idPop);
-   popupStyles.classList.add('popup_opened');
+function openPopUp(idPop) {
+   idPop.classList.add('popup_opened');
 };
-function popUpClosed(idPop) {
-   const popupStyles = document.querySelector(idPop);
-   popupStyles.classList.remove('popup_opened');
+function closePopUp(buttonNoSave) {
+   buttonNoSave.parentElement.parentElement.classList.remove('popup_opened');
 };
 
 // Создание первоначальных картинок
-function addCardElement(elementArrCard) {
+function createCard(elementArrCard) {
    const cardFrame = cardTemplate.cloneNode(true);
+      
    cardFrame.querySelector('.element__image').src = elementArrCard.link;
    cardFrame.querySelector('.element__info-text').textContent = elementArrCard.name;
    cardsPosition.prepend(cardFrame);
+   return cardFrame;
+};
 
-   arrPictures = document.querySelector('.element');
-   
-   const buttonLiked = arrPictures.querySelector('.element__button-liked');
-   const buttonTrash = arrPictures.querySelector('.element__basura');
-   const buttonImage = arrPictures.querySelector('.element__image');
+function addCardElement(cardFrame) {
+   const buttonLiked = cardFrame.querySelector('.element__button-liked');
+   const buttonTrash = cardFrame.querySelector('.element__basura');
+   const buttonImage = cardFrame.querySelector('.element__image');
+   const picInfo = cardFrame.querySelector('.element__info');
          
    buttonLiked.addEventListener('click', function() {
          buttonLiked.classList.toggle('element__button-liked_activ');
    });
-
    buttonTrash.addEventListener('click', function() {
       const listItem = buttonTrash.closest('.element');
          listItem.remove();         
       });
    buttonImage.addEventListener('click', function() {
-      const popName = document.querySelector('#popShowPicture');
-      const buttonNoSave = popName.querySelector('.popup__nosave-button');
+      openPopPic.querySelector('.popup__bigpicture').src = buttonImage.src;
+      openPopPic.querySelector('.popup__bigpicture').alt = 'Большая картинка';
+      openPopPic.querySelector('.popup__text').textContent = picInfo.textContent;
 
-      popName.querySelector('.popup__bigpicture').src = elementArrCard.link;
-      popName.querySelector('.popup__text').textContent = elementArrCard.name;
-
-      popUpOpened('#popShowPicture');
-      buttonNoSave.addEventListener('click', function() {
-         popUpClosed('#popShowPicture');
-      });
+      openPopUp(openPopPic);
    });
-   
+};
+
+for (let i = 0; i < buttonClosePopupPicture.length; i++) {
+   buttonClosePopupPicture[i].addEventListener('click', function() {
+      closePopUp(buttonClosePopupPicture[i]);
+   });
 };
 
 for (let i = initialCards.length - 1; i >= 0; i--) {
-   addCardElement(initialCards[i]);
+   addCardElement(createCard(initialCards[i]));
 };
 
 // Редактирование Кусто
 buttonEdit.addEventListener('click', function() {
-   const popName = document.querySelector('#popEdit');
-
    const nameCh = document.querySelector('#change-name');
    const infoCh = document.querySelector('#change-profeccion');
+   const formChange = openPopInfo.querySelector('.profile-change');
 
-   const buttonNoSave = popName.querySelector('.popup__nosave-button');
-   const formChange = popName.querySelector('.profile-change');
+   openPopUp(openPopInfo);
 
-   popUpOpened('#popEdit');
-
-   buttonNoSave.addEventListener('click', function() {
-      popUpClosed('#popEdit');
-   });
    nameCh.value = nameInKusto.textContent.trim();
    infoCh.value = infoInKusto.textContent.trim();
     
@@ -111,22 +81,24 @@ buttonEdit.addEventListener('click', function() {
       event.preventDefault();
       nameInKusto.textContent = nameCh.value;
       infoInKusto.textContent = infoCh.value;
-      popUpClosed('#popEdit');
+      openPopInfo.classList.remove('popup_opened');
    });
 });
 
 // Вставка новой карточки
 buttonAddPicture.addEventListener('click', function() {
-   const popName = document.querySelector('#popAddPicture');
-   const buttonNoSave = popName.querySelector('.popup__nosave-button');
-      
-   popUpOpened('#popAddPicture');
-   buttonNoSave.addEventListener('click', function() {
-      popUpClosed('#popAddPicture');      
-   });
+   openPopUp(openPopCard);
 });
 formAddCard.addEventListener('submit', function() {
    event.preventDefault();
-   addCardElement({name: nameInCard.value, link: infoInCard.value});
-   popUpClosed('#popAddPicture');
+
+   if (nameInCard.value == '' || infoInCard.value == '') {
+      nameInCard.value = 'Байкал как пример';
+      infoInCard.value = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg';
+      openPopCard.classList.remove('popup_opened');
+   }
+   addCardElement(createCard({name: nameInCard.value, link: infoInCard.value}));
+   nameInCard.value = 'Название';
+   infoInCard.value = 'Адрес картинки';
+   openPopCard.classList.remove('popup_opened');
 });
